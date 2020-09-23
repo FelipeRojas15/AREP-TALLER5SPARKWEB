9f1744a054e7cc46660f00eded1dfc2bd28fda69
Felipe Rojas <brayan.rojas-b@mail.escuelaing.edu.co>
2020-09-22 23:38:52 -0500

    Agregando JavaDoc

--- src/main/java/edu/escuelaing/arem/app/sparkWeb/controller/SparkWeb.java
+++ src/main/java/edu/escuelaing/arem/app/sparkWeb/controller/SparkWeb.java
@@ -3,7 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package edu.escuelaing.arem.app.sparkWeb;
+package edu.escuelaing.arem.app.sparkWeb.controller;
+import edu.escuelaing.arem.app.sparkWeb.persistence.DataStub;
 import edu.escuelaing.arem.app.*;
 import java.io.IOException;
 import static spark.Spark.*;
@@ -40,6 +41,11 @@ public class SparkWeb {
 
         return "";
     }
+
+    /**
+     * Indica el puerto por donde se va a comunicar el servidor
+     * @return int Puerto
+     */
     public static int getPort(){
         if (System.getenv("PORT") != null) {
             return Integer.parseInt(System.getenv("PORT"));
--- src/main/java/edu/escuelaing/arem/app/sparkWeb/persistence/DataStub.java
+++ src/main/java/edu/escuelaing/arem/app/sparkWeb/persistence/DataStub.java
@@ -1,4 +1,4 @@
-package edu.escuelaing.arem.app.sparkWeb;
+package edu.escuelaing.arem.app.sparkWeb.persistence;
 
 import com.mongodb.DBCollection;
 import com.mongodb.DBObject;
@@ -14,12 +14,18 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
+/**
+ * Conexion a la base de datos y sus respectivas acciones como insertar y seleccionar los datos
+ */
 public class DataStub {
 
     private MongoClientURI uri;
     private MongoClient mongoClient;
     private MongoDatabase mongodb;
 
+    /**
+     * Conexion a la base de datos
+     */
     public DataStub() {
         uri = new MongoClientURI(
                 "mongodb://admin:admin123@database-arep:27017/?serverSelectionTimeoutMS=5000&connectTimeoutMS=10000&authSource=database&authMechanism=SCRAM-SHA-256&3t.uriVersion=3&3t.connection.name=database");
@@ -27,6 +33,11 @@ public class DataStub {
         mongodb = mongoClient.getDatabase("database");
 
     }
+
+    /**
+     * Insertar Mensaje a la base de datos por medio de documentos
+     * @param mensaje, mensaje a insertar
+     */
     public void insertData(String mensaje){
         System.out.println("Aca voy");
         Date fecha = new Date();
@@ -36,6 +47,11 @@ public class DataStub {
         Document documento= new Document(dataMap);
         getMongodb().getCollection("datosArep").insertOne(documento);
     }
+
+    /**
+     * Trae los datos de la base de datos como string
+     * @return String con los datos
+     */
     public String getData(){
 
         MongoCollection<Document> coleccion =  getMongodb().getCollection("datosArep");
@@ -47,6 +63,10 @@ public class DataStub {
 
         return data;
     }
+    /**
+     * trae la base de datos
+     * @return MongoDatabase, base de datos
+     */
     public MongoDatabase getMongodb() {
         return mongodb;
     }
